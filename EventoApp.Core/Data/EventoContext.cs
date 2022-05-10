0e1f526e8abd9da7011688e668f743621c7c8be3
CARLOS EDUARDO <91492107+Carloseduardoviso@users.noreply.github.com>
2022-05-10 19:59:57 -0400

    Add files via upload

---
+++ EventoApp.Core/Data/EventoContext.cs
@@ -0,0 +1,18 @@
+﻿using EventoApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventoApp.Core.Data;
+
+public class EventoContext : DbContext
+{
+    public DbSet<Evento> Eventos { get; set; } = default!;
+    public EventoContext(DbContextOptions<EventoContext> options)
+        : base(options)
+    {
+    }
+}
